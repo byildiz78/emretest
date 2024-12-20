df512db2893da50bb59f81a4453eef3222adb45b
Emre SUALP <esualp95@gmail.com>
2024-12-20 10:42:17 +0300

    şlkfsd

--- next.config.mjs
+++ next.config.mjs
@@ -6,14 +6,13 @@ const nextConfig = {
       defaultLocale: 'tr',
     },
     output: 'standalone',
+    eslint: {
+        ignoreDuringBuilds: true,
+      },
       typescript: {
         // ⚠️ TypeScript hatalarını görmezden gel
         ignoreBuildErrors: true,
-      },
-      eslint: {
-        // ⚠️ ESLint hatalarını görmezden gel
-        ignoreDuringBuilds: true,
-      },
+      }
 };
 
 export default nextConfig;
